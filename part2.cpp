2e631eddf17cb23e3635152265d93541df7be030
Guadalupemag <gmagdaleno@horizon.csueastbay.edu>
2021-08-25 20:15:26 -0700

    Completed part 2. Initial commit to check against auto grader.

---
+++ part2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "C++ is fun!!!" << endl;
+    cout << "'_'" << endl;
+    cout << "Are you having fun yet?" << endl;
+    return 0;
+}
