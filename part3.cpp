cbd7e198a5e5ca94ff692b427b97485333ec1335
Guadalupemag <gmagdaleno@horizon.csueastbay.edu>
2021-08-25 20:26:26 -0700

    Completed part 3. Initial commit to check against auto grader.

---
+++ part3.cpp
@@ -0,0 +1,12 @@
+//Copy-paste this code into part3.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "   *" << endl;
+    cout << "  ***" << endl;
+    cout << " *****" << endl;
+    cout << "*******" << endl;
+    return 0;
+}
